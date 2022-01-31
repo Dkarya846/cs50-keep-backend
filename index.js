162a6eabe068b471dcc08aec7f76aae21d38fb5a
Deepak Kumar <dkarya.delhi.uk@gmail.com>
2022-01-31 23:30:26 +0530

    cors fix

--- index.js
+++ index.js
@@ -10,13 +10,19 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(function (req, res, next) {
-   res.header("Access-Control-Allow-Origin", "https://cs50-keep.herokuapp.com"); // update to match the domain you will make the request from
-   res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-   );
-   next();
+// app.use(function (req, res, next) {
+//    res.header("Access-Control-Allow-Origin", "https://cs50-keep.herokuapp.com"); // update to match the domain you will make the request from
+//    res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//    );
+//    next();
+// });
+app.use(cors());
+
+app.use((req, res, next) => {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Methods", "POST,GET,LINK");
 });
 
 if (!config.get("jwtPrivateKey")) {
