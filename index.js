4cb02640289f069689d3341eafee079bb000a42a
Deepak Kumar <dkarya.delhi.uk@gmail.com>
2022-01-31 23:05:37 +0530

    cors fix

--- index.js
+++ index.js
@@ -10,13 +10,11 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-
-const corsOptions = {
-   origin: "*",
-   credentials: true, //access-control-allow-credentials:true
-   optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", ""); // update to match the domain you will make the request from
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+ });
 
 if (!config.get("jwtPrivateKey")) {
    console.log("Fatal Error: Secret Key not provided");
