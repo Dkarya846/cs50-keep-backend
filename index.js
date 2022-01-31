04fd7eec272b1a0835cd1fd75f5f8a699a5f9349
Deepak Kumar <dkarya.delhi.uk@gmail.com>
2022-01-31 21:10:23 +0530

    added mongodb databse

--- index.js
+++ index.js
@@ -12,20 +12,30 @@ app.use(express.json());
 app.use(cors());
 
 if (!config.get("jwtPrivateKey")) {
-  console.log("Fatal Error: Secret Key not provided");
-  process.exit(1);
+   console.log("Fatal Error: Secret Key not provided");
+   process.exit(1);
 }
 
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/api/notes", notes);
 
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+
+if (!dbPass || !dbUser) {
+   console.log("Database User or Password not provided");
+   process.exit();
+}
+
 mongoose
-  .connect("mongodb://localhost:27017/cs50Keeper")
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB..."));
+   .connect(
+      `mongodb+srv://${dbUser}:${dbPass}@cluster0.rnqug.mongodb.net/cs50keep?retryWrites=true&w=majority`
+   )
+   .then(() => console.log("Connected to MongoDB..."))
+   .catch((err) => console.error("Could not connect to MongoDB...", err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server started at ${port}`);
+   console.log(`Server started at ${port}`);
 });
